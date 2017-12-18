3197dfea86d7e17ec5151f3b040fa17499bee570
yinxinxin <1278878921@qq.com>
2017-12-18 17:30:31 +0800

    简历

---
+++ sources/Application/Wap/Controller/ResumeController.class.php
@@ -0,0 +1,26 @@
+<?php
+// +----------------------------------------------------------------------
+// | OneThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
+// +----------------------------------------------------------------------
+
+namespace Wap\Controller;
+use OT\DataDictionary;
+
+/**
+ * 前台首页控制器
+ * 主要获取首页聚合数据
+ */
+class ResumeController extends HomeController {
+	//通知列表
+    public function resume(){
+		$this->display('resume');
+	}
+	
+	public function edit(){
+		$this->display('edit');
+	}
+}
