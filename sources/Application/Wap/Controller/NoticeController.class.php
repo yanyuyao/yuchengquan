3f307760bf28f1e348337f4fb227b05f67ffa62d
yinxinxin <1278878921@qq.com>
2017-12-18 15:25:10 +0800

    系统通知-列表页、详情页

---
+++ sources/Application/Wap/Controller/NoticeController.class.php
@@ -0,0 +1,26 @@
+<?php
+// +----------------------------------------------------------------------
+// | OneThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
+// +----------------------------------------------------------------------
+
+namespace Wap\Controller;
+use OT\DataDictionary;
+
+/**
+ * 前台首页控制器
+ * 主要获取首页聚合数据
+ */
+class NoticeController extends HomeController {
+	//通知列表
+    public function noticelist(){
+		$this->display('noticelist');
+	}
+	
+	public function noticeinfo(){
+		$this->display('noticeinfo');
+	}
+}
