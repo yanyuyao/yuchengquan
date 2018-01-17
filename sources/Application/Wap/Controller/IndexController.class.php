02bffd8c1e8afb89bdc2f5b2b96e7b9c9b7336f6
yinxinxin <1278878921@qq.com>
2018-01-17 17:18:21 +0800

    index页面job列表

--- sources/Application/Wap/Controller/IndexController.class.php
+++ sources/Application/Wap/Controller/IndexController.class.php
@@ -18,7 +18,9 @@ class IndexController extends Home2Controller {
 
 	//系统首页
     public function index(){
-
+		$jobnum = isset($_REQUEST["jobnum"])?$_REQUEST["jobnum"]:0;
+		
+		
         $category = D('Category')->getTree();
         $lists    = D('Document')->lists(null);
 
@@ -27,10 +29,19 @@ class IndexController extends Home2Controller {
         $this->assign('page',D('Document')->page);//分页
         
         $user_auth = session('user_auth');
-         
-        $home_quanzhi_job = M('job')->where("status = 1 and ishome = 1 and job_type = '全职'")->select(); //
-        $home_jianzhi_job = M('job')->where("status = 1 and ishome = 1 and job_type = '兼职'")->select();
         
+        $home_quanzhi_job = M('job')->where("status = 1 and ishome = 1 and job_type = '全职'")->limit("$jobnum,5")->select(); //
+        $home_jianzhi_job = M('job')->where("status = 1 and ishome = 1 and job_type = '兼职'")->limit("$jobnum,5")->select();
+        
+		if($jobnum != 0){
+			if($_REQUEST["jobtype"] == 0){
+				echo json_encode($home_quanzhi_job);
+			}else if($_REQUEST["jobtype"] == 1){
+				echo json_encode($home_jianzhi_job);
+			}
+			exit;
+		}
+		
         $this->assign("home_quanzhi_job",$home_quanzhi_job);
         $this->assign("home_jianzhi_job",$home_jianzhi_job);
         $this->display();
